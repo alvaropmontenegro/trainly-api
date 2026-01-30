23db442b27d0c5bcd6bdff89004bad1e970c47b0
Gustavo <montenegro12gustavo@gmail.com>
2026-01-30 20:06:48 -0300

    add IMemberRepository. add MembersRepository

--- Trainly.Domain/Interfaces/IMembersRepository.cs
+++ Trainly.Domain/Interfaces/IMembersRepository.cs
@@ -3,9 +3,9 @@ namespace Trainly.Domain.Interfaces;
 
 public interface IMembersRepository
 {
-    Task<Member?> GetByIdAsync(int id);
-    Task<IEnumerable<Member>> GetAllAsync();
+    //Task<Member?> GetByIdAsync(int id);
+    //Task<IEnumerable<Member>> GetAllAsync();
     Task<Member> AddAsync(Member member);
-    Task UpdateAsync(Member member);
-    Task DeleteAsync(int id);
+    //Task UpdateAsync(Member member);
+    //Task DeleteAsync(int id);
 }
