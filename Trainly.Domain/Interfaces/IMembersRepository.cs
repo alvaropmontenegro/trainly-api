a219edd261c3c1cbd14a11c7daa3b9cd23771c9f
Gustavo <montenegro12gustavo@gmail.com>
2026-02-03 15:36:53 -0300

    adjusting IMembersRepository

--- Trainly.Domain/Interfaces/IMembersRepository.cs
+++ Trainly.Domain/Interfaces/IMembersRepository.cs
@@ -3,9 +3,5 @@ namespace Trainly.Domain.Interfaces;
 
 public interface IMembersRepository
 {
-    //Task<Member?> GetByIdAsync(int id);
-    //Task<IEnumerable<Member>> GetAllAsync();
     Task<Member> AddAsync(Member member);
-    //Task UpdateAsync(Member member);
-    //Task DeleteAsync(int id);
 }
