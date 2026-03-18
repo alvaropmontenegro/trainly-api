89a010b531d31334eca012db16aa352e4a177c33
Gustavo <montenegro12gustavo@gmail.com>
2026-03-18 11:49:00 -0300

    minimal adjutings in Handlers of Member and Tenant

--- Trainly.Application/Commands/Members/CreateMembersHandler.cs
+++ Trainly.Application/Commands/Members/CreateMembersHandler.cs
@@ -2,9 +2,11 @@ using Microsoft.Extensions.Logging;
 using Trainly.Application.DTOs;
 using Trainly.Domain.Interfaces;
 using Trainly.Domain.Entities;
+using Trainly.Application.Interfaces;
+
 namespace Trainly.Application.Commands.Members;
 
-public class InsertMembersHandler
+public class InsertMembersHandler : ICommandHandler<InsertMemberCommand, MembersDto>
 {
     private readonly IMembersRepository _repository;
     private readonly ILogger<InsertMembersHandler> _logger;
--- Trainly.Application/Commands/Tenants/CreateTenantHandler.cs
+++ Trainly.Application/Commands/Tenants/CreateTenantHandler.cs
@@ -1,56 +1,56 @@
-using Microsoft.Extensions.Logging;
-using Trainly.Application.Commands.Tenants;
-using Trainly.Application.DTOs;
-using Trainly.Application.Interfaces;
-using Trainly.Domain.Entities;
-using Trainly.Domain.Interfaces;
-namespace Trainly.Application.Commands.Tenants;
+// using Microsoft.Extensions.Logging;
+// using Trainly.Application.Commands.Tenants;
+// using Trainly.Application.DTOs;
+// using Trainly.Application.Interfaces;
+// using Trainly.Domain.Entities;
+// using Trainly.Domain.Interfaces;
+// namespace Trainly.Application.Commands.Tenants;
 
-public class InsertTenantHandler : ICommandHandler<InsertTenantCommand, TenantDto>
-{
-    private readonly ITenantRepository _repository;
-    private readonly ILogger<InsertTenantHandler> _logger;
-    public InsertTenantHandler(ITenantRepository repository, ILogger<InsertTenantHandler> logger)
-    {
-        _repository = repository;
-        _logger = logger;
-    }
-    public async Task<TenantDto> Handle(InsertTenantCommand command)
-    {
-        _logger.LogInformation("Iniciando Inserção de Centro de Treinamento: {TenantName}", command.Name);
+// public class InsertTenantHandler : ICommandHandler<InsertTenantCommand, TenantDto>
+// {
+//     private readonly ITenantRepository _repository;
+//     private readonly ILogger<InsertTenantHandler> _logger;
+//     public InsertTenantHandler(ITenantRepository repository, ILogger<InsertTenantHandler> logger)
+//     {
+//         _repository = repository;
+//         _logger = logger;
+//     }
+//     public async Task<TenantDto> Handle(InsertTenantCommand command)
+//     {
+//         _logger.LogInformation("Iniciando Inserção de Centro de Treinamento: {TenantName}", command.Name);
 
-        if (string.IsNullOrWhiteSpace(command.Name))
-        {
-            _logger.LogError("Tentativa de criar Centro de Treinamento sem nome");
-            throw new ArgumentException("O nome do Centro de Treinamento é obrigatório", nameof(command.Name));
-        }
+//         if (string.IsNullOrWhiteSpace(command.Name))
+//         {
+//             _logger.LogError("Tentativa de criar Centro de Treinamento sem nome");
+//             throw new ArgumentException("O nome do Centro de Treinamento é obrigatório", nameof(command.Name));
+//         }
 
-        var newTenant = new Tenant
-        {
-            Name = command.Name,
-            Email = command.Email,
-            Phone = command.Phone,
-            Address = command.Address,
-            Plan = command.Plan,
-            PlanExpirationDate = command.PlanExpirationDate,
-            Language = command.Language,
-            Theme = command.Theme
-        };
+//         var newTenant = new Tenant
+//         {
+//             Name = command.Name,
+//             Email = command.Email,
+//             Phone = command.Phone,
+//             Address = command.Address,
+//             Plan = command.Plan,
+//             PlanExpirationDate = command.PlanExpirationDate,
+//             Language = command.Language,
+//             Theme = command.Theme
+//         };
 
-        var insertTenant = await _repository.AddAsync(newTenant);
+//         var insertTenant = await _repository.AddAsync(newTenant);
 
-        _logger.LogInformation("Centro de Treinamento Inserido com sucesso. ID: {TenantId}", insertTenant.Id);
-        return new TenantDto
-        {
-            Id = insertTenant.Id,
-            Name = insertTenant.Name,
-            Email = insertTenant.Email,
-            Phone = insertTenant.Phone,
-            Address = insertTenant.Address,
-            Plan = insertTenant.Plan,
-            PlanExpirationDate = insertTenant.PlanExpirationDate,
-            Language = insertTenant.Language,
-            Theme = insertTenant.Theme
-        };
-    }
-}
+//         _logger.LogInformation("Centro de Treinamento Inserido com sucesso. ID: {TenantId}", insertTenant.Id);
+//         return new TenantDto
+//         {
+//             Id = insertTenant.Id,
+//             Name = insertTenant.Name,
+//             Email = insertTenant.Email,
+//             Phone = insertTenant.Phone,
+//             Address = insertTenant.Address,
+//             Plan = insertTenant.Plan,
+//             PlanExpirationDate = insertTenant.PlanExpirationDate,
+//             Language = insertTenant.Language,
+//             Theme = insertTenant.Theme
+//         };
+//     }
+// }
